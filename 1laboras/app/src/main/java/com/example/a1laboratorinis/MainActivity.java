f5a9b12755b5438fa38d7ad0cff255fcffe50f82
Eneta Uzemeckaite <eneta.uzemeckaite@knf.stud.vu.lt>
2021-04-13 21:54:57 +0300

    Added button and text elements

--- 1laboras/app/src/main/java/com/example/a1laboratorinis/MainActivity.java
+++ 1laboras/app/src/main/java/com/example/a1laboratorinis/MainActivity.java
@@ -3,12 +3,22 @@ package com.example.a1laboratorinis;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textfield;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.textfield = findViewById(R.id.textfield);
+    }
+
+    public void click_mainbutton(View view) {
+        this.textfield.setText("Have a nice day!");
     }
 }
