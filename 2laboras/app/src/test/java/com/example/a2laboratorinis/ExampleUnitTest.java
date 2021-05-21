c884012e0615b74c86b87f9416be7c6d4aa4b289
Eneta Uzemeckaite <eneta.uzemeckaite@knf.stud.vu.lt>
2021-05-21 19:21:08 +0300

    added unit tests for SymbolsWordCounter

--- 2laboras/app/src/test/java/com/example/a2laboratorinis/ExampleUnitTest.java
+++ 2laboras/app/src/test/java/com/example/a2laboratorinis/ExampleUnitTest.java
@@ -10,8 +10,94 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class ExampleUnitTest {
+
+    @Test(expected = NullPointerException.class)
+    public void nullIsGivenTo_getSymbolsCount(){
+        SymbolsWordsCounter.getSymbolsCount(null);
+    }
+
+    @Test(expected = NullPointerException.class)
+    public void nullIsGivenTo_getWordsCount(){
+        SymbolsWordsCounter.getWordsCount(null);
+    }
+
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void emptyIsGivenTo_getSymbolsCount(){
+        final int result = 0;
+        final int functionResult = SymbolsWordsCounter.getSymbolsCount("");
+
+        assertEquals(result, functionResult);
+    }
+
+    @Test
+    public void emptyIsGivenTo_getWordsCount(){
+        final int result = 0;
+        final int functionResult = SymbolsWordsCounter.getWordsCount("");
+
+        assertEquals(result, functionResult);
+    }
+
+    @Test
+    public void spacesIsGivenTo_getSymbolsCount(){
+        final int result = 4;
+        final int functionResult = SymbolsWordsCounter.getSymbolsCount("    ");
+
+        assertEquals(result, functionResult);
+    }
+
+    @Test
+    public void spacesIsGivenTo_getWordsCount(){
+        final int result = 0;
+        final int functionResult = SymbolsWordsCounter.getWordsCount("    ");
+
+        assertEquals(result, functionResult);
+    }
+
+    @Test
+    public void normalIsGivenTo_getSymbolsCount(){
+        final int result = 11;
+        final int functionResult = SymbolsWordsCounter.getSymbolsCount("Hello World");
+
+        assertEquals(result, functionResult);
+    }
+
+    @Test
+    public void normalIsGivenTo_getWordsCount(){
+        final int result = 2;
+        final int functionResult = SymbolsWordsCounter.getWordsCount("Hello World");
+
+        assertEquals(result, functionResult);
+    }
+
+    @Test
+    public void symbolsIsGivenTo_getSymbolsCount(){
+        final int result = 23;
+        final int functionResult = SymbolsWordsCounter.getSymbolsCount("*/-+,.][()&^%$#@!`':;{}");
+
+        assertEquals(result, functionResult);
+    }
+
+    @Test
+    public void symbolsIsGivenTo_getWordsCount(){
+        final int result = 1;
+        final int functionResult = SymbolsWordsCounter.getWordsCount("*/-+,.][()&^%$#@!`':;{}");
+
+        assertEquals(result, functionResult);
+    }
+
+    @Test
+    public void numberIsGivenTo_getSymbolsCount(){
+        final int result = 10;
+        final int functionResult = SymbolsWordsCounter.getSymbolsCount("1887465214");
+
+        assertEquals(result, functionResult);
+    }
+
+    @Test
+    public void numberIsGivenTo_getWordsCount(){
+        final int result = 1;
+        final int functionResult = SymbolsWordsCounter.getWordsCount("1887465214");
+
+        assertEquals(result, functionResult);
     }
 }
